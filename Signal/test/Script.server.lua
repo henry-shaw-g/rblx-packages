14e56a50bec4d7510c5ce5d7dbf0dd9e4822141c
Henry Shaw <henryshaw747@gmail.com>
2022-10-15 20:43:06 -0700

    Signal add more tests.

--- Signal/test/Script.server.lua
+++ Signal/test/Script.server.lua
@@ -2,16 +2,21 @@ local Signal = require(game.ReplicatedStorage.Signal)
 
 local testSignal = Signal.new()
 
+-- test basic firing
+print("Testing :connect, :fire.")
 local testConn = testSignal:connect(function()
     print("omg fired :o")
 end)
-
 testSignal:fire()
 
+-- test disconnecting
+print("\nTesting :disconnect.")
 testConn:disconnect()
 testSignal:fire()
 print("omg did not fire :o")
 
+-- test disconnecting in fire & multiple events
+print("\nTesting multiple connections & in-callback :disconnect.")
 local testConn2
 testConn2 = testSignal:connect(function() 
     print("omg only firing once :o")
@@ -22,3 +27,21 @@ testSignal:connect(function()
 end)
 testSignal:fire()
 testSignal:fire()
+
+-- test :wait()
+print("\nTesting :wait.")
+local testSignal2 = Signal.new()
+task.spawn(function() 
+    local message = testSignal2:wait()
+    print(message)
+end)
+testSignal2:fire(":wait success.")
+
+-- test :once()
+print("\nTest :once")
+local testSignal3 = Signal.new()
+testSignal3:once(function() 
+    print("Only once again :O")
+end)
+testSignal3:fire()
+testSignal3:fire()
