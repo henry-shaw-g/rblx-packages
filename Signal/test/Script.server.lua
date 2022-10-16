ca74ee8b46533ef192ece99bd8e7a9484ac27844
Henry Shaw <henryshaw747@gmail.com>
2022-10-15 20:12:35 -0700

    Add more signal tests.

--- Signal/test/Script.server.lua
+++ Signal/test/Script.server.lua
@@ -2,8 +2,23 @@ local Signal = require(game.ReplicatedStorage.Signal)
 
 local testSignal = Signal.new()
 
-testSignal:connect(function()
+local testConn = testSignal:connect(function()
     print("omg fired :o")
 end)
 
+testSignal:fire()
+
+testConn:disconnect()
+testSignal:fire()
+print("omg did not fire :o")
+
+local testConn2
+testConn2 = testSignal:connect(function() 
+    print("omg only firing once :o")
+    testConn2:disconnect()
+end)
+testSignal:connect(function() 
+    print("hmm")
+end)
+testSignal:fire()
 testSignal:fire()
